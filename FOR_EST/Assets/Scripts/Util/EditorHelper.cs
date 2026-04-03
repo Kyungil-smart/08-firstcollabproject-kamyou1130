30063ce14a2da2aa7fe3bde1da06210fcd90a168
baewhv <baewhv@naver.com>
2026-04-03 18:38:57 +0900

    [FIX] : Helper 디자인 수정

--- FOR_EST/Assets/Scripts/Util/EditorHelper.cs
+++ FOR_EST/Assets/Scripts/Util/EditorHelper.cs
@@ -5,26 +5,33 @@ using UnityEngine;
 
 public class EditorHelper : MonoBehaviour
 {
-    GUIStyle style = new GUIStyle();
+    private GUIStyle style;
+
     private void OnDrawGizmos()
     {
-        Vector3 size = new Vector3(2.5f, 1.5f, 1.0f);
-        Vector3 pos = new Vector3(size.x * 0.5f, -size.y * 0.5f, 0.0f);
-        
-        Gizmos.color = new Color(0,0,0,0.5f);
-        Gizmos.DrawCube(transform.position + pos, size);
-        
-        Gizmos.color = Color.green;
-        Gizmos.DrawSphere(transform.position,0.5f);
-        
-        style.normal.textColor = Color.red;
-        Handles.Label(transform.position, $"x : {transform.position.x}\ny : {transform.position.y}", style);
+        Vector3 size = new Vector3(0.5f, -0.5f, 0.0f);
+
+        Gizmos.color = new Color(0, 1, 0, 0.5f);
+        Gizmos.DrawSphere(transform.position, 0.3f);
+
+
+        style = new GUIStyle(GUI.skin.box)
+        {
+            normal =
+            {
+                textColor = Color.red
+            },
+            alignment = TextAnchor.UpperLeft
+        };
+
+        Handles.Label(transform.position+ size, $"x : {transform.position.x}\ny : {transform.position.y}", style);
     }
 
     public void StartDestroy()
     {
         StartCoroutine(Destroyer());
     }
+
     private IEnumerator Destroyer()
     {
         yield return YieldContainer.WaitForSeconds(3.0f);
